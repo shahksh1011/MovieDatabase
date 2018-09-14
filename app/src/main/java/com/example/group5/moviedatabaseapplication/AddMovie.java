2f92f488046f7358e4a3f5caa1533cf4c78c5bf9
kshitijshah <shahksh1011@gmail.com>
2018-09-14 18:15:59 -0400

    res edit

---
+++ app/src/main/java/com/example/group5/moviedatabaseapplication/AddMovie.java
@@ -0,0 +1,13 @@
+package com.example.group5.moviedatabaseapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_movie);
+    }
+}

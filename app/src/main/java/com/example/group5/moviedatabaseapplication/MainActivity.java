e68a687a5f6ea9e3537674eeab884ff3192a80a6
kshitijshah <shahksh1011@gmail.com>
2018-09-15 20:21:11 -0400

    edit

--- app/src/main/java/com/example/group5/moviedatabaseapplication/MainActivity.java
+++ app/src/main/java/com/example/group5/moviedatabaseapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.group5.moviedatabaseapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String str = "hello world";
+        Button addMovie = findViewById(R.id.addMovieButton);
+        Button editMovie = findViewById(R.id.editMovieButton);
+        Button deleteMovie = findViewById(R.id.deleteMovieButton);
+        Button moviesByYear = findViewById(R.id.sortMovieByYearButton);
+        Button moviesByRating = findViewById(R.id.sortMovieByRatingButton);
+
 
     }
 }

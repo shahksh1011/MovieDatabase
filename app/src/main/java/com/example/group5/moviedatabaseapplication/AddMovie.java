7e541f376ca3f1c488d1809765aea751e785e54a
kshitijshah <shahksh1011@gmail.com>
2018-09-16 16:37:28 -0400

    edit

--- app/src/main/java/com/example/group5/moviedatabaseapplication/AddMovie.java
+++ app/src/main/java/com/example/group5/moviedatabaseapplication/AddMovie.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.Serializable;
+import java.net.URL;
 
 public class AddMovie extends AppCompatActivity {
     @Override
@@ -24,6 +28,8 @@ public class AddMovie extends AppCompatActivity {
         final EditText year = findViewById(R.id.yearEdit);
         final SeekBar rating = findViewById(R.id.ratingSeekbar);
         final Spinner genreSpinner = findViewById(R.id.genreSpinner);
+        final TextView seekbarValue = findViewById(R.id.seekbarValueTextView);
+        seekbarValue.setText("0");
         Boolean flag = false;
         int p = 0;
         Button addMovie = findViewById(R.id.saveAddMovieButton);
@@ -39,18 +45,38 @@ public class AddMovie extends AppCompatActivity {
             genreSpinner.setSelection(m.getMovieGenre());
             rating.setProgress(m.getMovieRating());
             Log.d("Position", String.valueOf(position) + m.getName());
+            seekbarValue.setText(String.valueOf(rating.getProgress()));
             flag = true;
             p = position;
         }
+        rating.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekbarValue.setText(String.valueOf(rating.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         final int finalP = p;
+        final Boolean finalFlag = flag;
         addMovie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (name.getText().toString().matches("") | description.getText().toString().matches("") | imdb.getText().toString().matches("") | year.getText().toString().matches("")) {
+                if (name.getText().toString().matches("") | name.getText().toString().length()>50 | description.getText().toString().matches("") | imdb.getText().toString().matches("") | year.getText().toString().matches("") | !URLUtil.isValidUrl(imdb.getText().toString())) {
+                    new EditTextValidation(name,50,"Exceeded 50 character Limit", "Movie name can only have upto 50 characters", getApplicationContext());
                     new EditTextValidation(name, "Please Enter Name");
                     new EditTextValidation(description, "Please Enter Description");
                     new EditTextValidation(imdb, "Please Enter Imdb");
                     new EditTextValidation(year, "Please Enter Year");
+                    new EditTextValidation("Enter valid Url", imdb,"The IMDB Url is invalid", getApplicationContext());
                 } else {
 
                     Log.d("Hola", "woo");
@@ -58,7 +84,7 @@ public class AddMovie extends AppCompatActivity {
                     Intent i = new Intent(getApplicationContext(), MainActivity.class);
                     Movie m = new Movie(name.getText().toString(), description.getText().toString(), imdb.getText().toString(), (int) genreSpinner.getSelectedItemId(), Integer.parseInt(year.getText().toString()), rating.getProgress());
                     i.putExtra("MOVIES", (Serializable) m);
-                    if (true)
+                    if (finalFlag)
                         i.putExtra("Position", finalP);
                     startActivity(i);
                 }
--- app/src/main/java/com/example/group5/moviedatabaseapplication/EditTextValidation.java
+++ app/src/main/java/com/example/group5/moviedatabaseapplication/EditTextValidation.java
@@ -1,10 +1,27 @@
 package com.example.group5.moviedatabaseapplication;
 
+import android.content.Context;
+import android.webkit.URLUtil;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditTextValidation {
     EditTextValidation(EditText editText, String message) {
         if (editText.getText().toString().matches(""))
             editText.setError(message);
+    }
+    EditTextValidation(EditText editText, int value, String message, String toast, Context context){
+        if (editText.getText().toString().length() > value){
+            editText.setError(message);
+            Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
+        }
+
+    }
+    EditTextValidation(String message, EditText editText, String toast, Context context){
+        if (!URLUtil.isValidUrl(editText.getText().toString())){
+            editText.setError(message);
+            Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
